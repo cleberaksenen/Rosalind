6719170bf87e2af8b1defb9c6d5eeb02eb91f167
cleberaksenen <cleber.aksenen@fiocruz.br>
2023-03-15 11:25:20 -0300

    Counting DNA Nucleotides

---
+++ rosalind01.py
@@ -0,0 +1,4 @@
+#Algoritmo de resolução do primeiro desafio do Rosalind
+#Counting DNA Nucleotides
+DNA = input("Digite sua sequência de nucleotídeos: ").upper()
+print(DNA.count("A"), DNA.count("C"), DNA.count("G"), DNA.count("T"))
